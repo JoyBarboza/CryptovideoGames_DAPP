01d1637c07773c43570207d51b6500e88cbd39e7
wilmervn83 <69256406+wilmervn83@users.noreply.github.com>
2020-08-06 11:39:25 -0300

    Add files via upload

---
+++ ads.php
@@ -0,0 +1,19 @@
+<?php
+
+$adsheat = '
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- adsmanual -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3112529156459266"
+     data-ad-slot="5666173387"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+';
+
+
+
+
+
+
+?>
